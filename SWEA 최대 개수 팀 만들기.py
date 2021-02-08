8b16d888db2eacc467c5a9e76020a63d4fd58bd6
Jangbeomjin <qjawls0501@naver.com>
2021-02-08 15:22:17 +0900

    02/08-1

--- SWEA 최대 개수 팀 만들기.py
+++ SWEA 최대 개수 팀 만들기.py
@@ -11,53 +11,43 @@ def bfs(x, y):
         for d in range(4):
             nx = cx + dx[d]
             ny = cy + dy[d]
-            if 0 <= nx < N and 0 <= ny < N and area[nx][ny] >= 1 and visit[nx][ny] == 0:
+            if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] >= 1 and visit[nx][ny] == 0:
                 visit[nx][ny] = 1
                 queue.append([nx, ny])
 
 
 def score():
-    global yearnum
-    yearnum += 1
     for i in range(N):
         for j in range(N):
-            if area1[i][j] >= 1:
-                area[i][j] -= 1
+            if graph[i][j] >= 1:
+                graph[i][j] -= 1
+
+TC = int(input())
+for t in range(TC):
+    N = int(input())
+    graph = [[0 for _ in range(N)] for _ in range(N)]
+    result = []
+    dx = [0, -1, 0, 1]
+    dy = [-1, 0, 1, 0]
+    yearnum = 0
+
     for i in range(N):
+        graph[i] = list(map(int, sys.stdin.readline().split()))
+
+
+    while True:
+        score()
+        count = 0
+        visit = [[0 for _ in range(N)] for _ in range(N)]
         for j in range(N):
-            area1[i][j] = area[i][j]
+            for k in range(N):
+                if graph[j][k] >= 1 and visit[j][k] == 0:
+                    bfs(j, k)
+                    count += 1
+        result.append(count)
+        if count == 0:
+            break
+    if max(result) == 0:
+        result.append(1)
 
-
-N = int(input())
-area = [[0 for _ in range(N)] for _ in range(N)]
-area1 = [[0 for _ in range(N)] for _ in range(N)]
-graph = [[0 for _ in range(N)] for _ in range(N)]
-result = []
-dx = [0, -1, 0, 1]
-dy = [-1, 0, 1, 0]
-yearnum = 0
-
-
-for i in range(N):
-    graph[i] = list(map(int, sys.stdin.readline().split()))
-
-area = copy.deepcopy(graph)
-area1 = copy.deepcopy(graph)
-
-
-while True:
-    score()
-    count = 0
-    visit = [[0 for _ in range(N)] for _ in range(N)]
-    for j in range(N):
-        for k in range(N):
-            if area[j][k] >= 1 and visit[j][k] == 0:
-                bfs(j, k)
-                count += 1
-    result.append(count)
-    if count == 0:
-        yearnum = 0
-        break
-
-
-print(max(result))
+    print("#%d %d"%(t+1, max(result)))
