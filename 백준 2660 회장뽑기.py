45d60b7f3ce6dd822840b955ec71f08a5fff10f1
Jangbeomjin <qjawls0501@naver.com>
2021-02-10 09:28:07 +0900

    02/09-last

--- 백준 2660 회장뽑기.py
+++ 백준 2660 회장뽑기.py
@@ -1,6 +1,10 @@
 member = int(input())
-rel =[[] for _ in range(member+1)]
-visit = [0 for _ in range(member+1)]
+rel =[[100 for _ in range(member+1)] for _ in range(member+1)]
+visit = [[0 for _ in range(member+1)] for _ in range(member+1)]
+hubo= []
+
+
+
 
 while True:
     a, b = map(int, input().split())
@@ -8,6 +12,10 @@ while True:
     if a == -1 and b == -1:
         break
     else:
-        rel[a].append(b)
+        rel[a][b] = 1
+        rel[b][a] = 1
 
+    for i in range(1, member+1):
+        if rel[i] == min(rel):
+            hubo.append(i)
 print(rel)
