73ba20d1af944353c7673a8a5ebc735c5db137e5
Jangbeomjin <qjawls0501@naver.com>
2021-02-17 21:39:01 +0900

    studyfolder remove

--- 백준 2651 자동차경주대회.py
+++
@@ -1,8 +0,0 @@
-max_distance = int(input())
-number = int(input())
-distance = list(map(int, input().split()))
-time =[0] + list(map(int, input().split())) + [0]
-mydistance = [0 for _ in range(number + 2)]
-
-print(distance)
-print(time)
