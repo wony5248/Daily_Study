09941c85b4e8b488e56a3d180ad5ac0f7fa865f7
Jangbeomjin <qjawls0501@naver.com>
2021-02-20 17:38:49 +0900

    02-20

--- 02-24/프로그래머스  K번째 수 -sorting.py
+++ 02-24/프로그래머스  K번째 수 -sorting.py
@@ -4,7 +4,7 @@ def solution(array, commands):
     answer = []
 
     for i in range(len(commands)):
-        lst = array[commands[i][0] -1:commands[i][1]]
+        lst = array[commands[i][0] -1:commands[i][1]]    # commands 가 i j k 일때 i부터 j 까지 split
         lst.sort()
-        answer.append(lst[commands[i][2]-1])
+        answer.append(lst[commands[i][2]-1])   # sort후에  k-1번째 인덱스 answer에 추가
     return answer
--- 02-24/프로그래머스 가장 큰 수-sorting.py
+++ 02-24/프로그래머스 가장 큰 수-sorting.py
@@ -5,7 +5,7 @@ def solution(numbers):
     answer = ''
     string = list(map(str, numbers))
     string.sort(key=lambda x: x * 3, reverse=True)    ## 문자열로 sort하면 자릿수 상관없이 맨앞숫자 큰순으로 정렬 => 문자열로 합칠때 최대 되는 순서
-    for i in range(len(string)):
+    for i in range(len(string)):                      ## *3 안하면 9 랑 97 들어왔을때 97이 더 크다고 인식  9랑 99랑 999랑 같게끔 해줘야 함
         answer += string[i]
     result = int(answer)
     answer = str(result)
