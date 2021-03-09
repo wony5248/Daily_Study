3daa84d4ad9cd1afd18fb2682cec9cdd3b857588
Jangbeomjin <qjawls0501@naver.com>
2021-03-10 00:31:00 +0900

    03-10

--- 03-10/백준 1062 가르침 - 완탐.py
+++ 03-10/백준 1062 가르침 - 완탐.py
@@ -2,23 +2,19 @@ import sys
 from itertools import combinations
 from string import ascii_lowercase
 N, K = map(int, sys.stdin.readline().split())
-string = set()
-for _ in range(N):
-    string.add(set(sys.stdin.readline().rstrip()[4:-4]).difference("a", "c", "i", "t", "n"))
+
+string = [(set(sys.stdin.readline().rstrip()[4:-4]).difference("a", "c", "i", "t", "n")) for _ in range(N)]
 tmp = set(ascii_lowercase).difference("a", "c", "i", "t", "n")
 result = 0
+
+# def solve():
+
+
 if K < 5:
     result = 0
 elif K == 26:
     result = N
-else:
-    for i in set(combinations(tmp, K-5)):
-        count = 0
-        for j in string:
-            if not j.difference(i):
-                count += 1
 
-        result = max(result, count)
 # 97~122
 
-print(result)
+print(tmp)
--- 03-10/백준 2178 미로탐색 - bfs.py
+++ 03-10/백준 2178 미로탐색 - bfs.py
@@ -7,6 +7,8 @@ result = [[0 for _ in range(M)] for _ in range(N)]
 visit = [[0 for _ in range(M)] for _ in range(N)]
 miro = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
 
+
+
 def bfs(x, y):                    # 0,0 부터 bfs 탐색 시작
     queue = deque()
     queue.append([x, y])
@@ -26,3 +28,39 @@ def bfs(x, y):                    # 0,0 부터 bfs 탐색 시작
 
 bfs(0, 0)
 print(result[N-1][M-1])
+
+
+# import sys
+# from collections import deque
+#
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+# def bfs(x, y, cnt):
+#     visited[x][y] = 1
+#     cnt+=1
+#     for i in range(4):
+#         xx = x+dx[i]
+#         yy = y+dy[i]
+#         if 0<=xx<n and 0<=yy<m and arr[xx][yy] == '1' and visited[xx][yy] == 0:
+#                 dq.append([xx, yy, cnt])
+#                 visited[xx][yy] = 1
+#
+#
+# n, m = map(int, sys.stdin.readline().split())
+# arr = []
+# for _ in range(n):
+#     arr.append(list(sys.stdin.readline().rstrip()))
+# visited = [[0]*m for _ in range(n)]
+# dq = deque()
+# dq.append([0,0,1])
+# # bfs(0, 0, 1)
+# result = 100001
+# while dq:
+#     x, y, cnt = dq.popleft()
+#     result = cnt
+#     if x==n-1 and y == m-1:
+#         break
+#     bfs(x, y, cnt)
+#     # print(dq)
+#
+# print(result)
