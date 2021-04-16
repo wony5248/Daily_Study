9bc25fb174fe823b1cd33b909ce190fe0f8116c9
wony5248 <qjawls0501@naver.com>
2021-04-17 00:06:36 +0900

    04-16

--- 04-21/백준 18870 좌표 압축.py
+++ 04-21/백준 18870 좌표 압축.py
@@ -1,1 +1,11 @@
+import sys
+input =sys.stdin.readline
 N = int(input())
+coor = list(map(int, input().split()))
+zipcoor= {}                         # 입력 받은 좌표의 크기 순서를 저장하는 dict()
+scoor = list(sorted(set(coor)))     # 중복 제거하기 위해 set 해주고 다시 list
+for i in range(len(scoor)):         # scoor[i]의 크기 순서는 i
+    zipcoor[scoor[i]] = i
+
+for i in coor:
+    print(zipcoor[i], end=" ")
--- 04-21/백준 5525 IOIOI.py
+++ 04-21/백준 5525 IOIOI.py
@@ -3,6 +3,17 @@ input = sys.stdin.readline
 N = int(input())
 M = int(input())
 P = input().rstrip()
-
-string = "IOIOI"
-print(string.count("IOI"))
+count = 0
+result = 0
+i = 0
+while i < M-2:
+    if P[i] == "I" and P[i+1] == "O" and P[i+2] == "I":   # IOI를 찾았을때
+        count += 1                # count 1 증가
+        if count == N:            # count가 N 이면  문자열이 있다는 거
+            count -= 1            # 맨앞 IOI 빼줌
+            result += 1           # 문자 갯수  하나 추가
+        i += 1                    # IOI면 다음 I로 가기위해 인덱스 1 더 추가
+    else:
+        count = 0                 # IOI가 아니면 카운트 0으로 초기화
+    i += 1
+print(result)
