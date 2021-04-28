4c95b2970cb8e3b7cf4b6c6bce768b2c51e6481c
wony5248 <qjawls0501@naver.com>
2021-04-28 22:02:38 +0900

    04-28

---
+++ 04-28 & 05-05/백준 16953 A - B.py
@@ -0,0 +1,21 @@
+A, B = list(map(int, input().split()))
+count = 1
+while B != A:
+    if B < A or (B % 2 != 0 and B % 10 !=1):
+        count = -1
+        break
+    elif B % 2 == 0:
+        B //= 2
+        count += 1
+    elif B % 2 != 0:
+        B //= 10
+        count += 1
+
+print(count)
+
+
+
+
+
+
+
