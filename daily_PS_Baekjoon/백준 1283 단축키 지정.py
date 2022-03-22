c6396174333aae648944dc43edb251606edf299b
Jangbeomjin <qjawls0501@naver.com>
2022-03-22 21:42:00 +0900

    03-22

--- daily_PS_Baekjoon/백준 1283 단축키 지정.py
+++ daily_PS_Baekjoon/백준 1283 단축키 지정.py
@@ -1,32 +1,12 @@
 import sys
-import math
-from functools import reduce
 input = sys.stdin.readline
+# dic = dict()
+dic = []
+N = int(input())
 
-N = int(input().rstrip())
-S = []
-
-def multiply(arrs):
-    return reduce(lambda x, y: x * y, arrs)
-
-def lcm(arrs):
-    return multiply(arrs) // math.gcd(*arrs)
-
-for _ in range(N):
-    S.append(input().rstrip()[2:])
-
-d = {}
-for i in range(1, 954):
-    for j in range(i):
-        t = f'{j/i:.4f}'[2:5]
-        if t not in d:
-            d[t] = i
-
-ans = []
-for v in S:
-    ans.append(d[v])
-
-
-
-
-print(lcm(ans))
+words = [input().rstrip().split() for _ in range(N)]
+for word in words:
+   print(word[0])
+    # if word[0] not in dic:
+    #     dic.append(word[0])
+print(words)
